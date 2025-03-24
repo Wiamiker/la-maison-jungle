f2bc91e2af7a5f1b175dfc491cc1a82b0b1cbac6
Ikerchalene Wiam <imounane.wiam@gmail.com>
2025-03-24 10:52:04 +0100

    entrainement react 1er cours

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import Banner from './Banner'
+import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+import CareScale from './CareScale'
+import QuestionForm from './QuestionForm'
+import Footer from './Footer'
+
+function App() {
+    return (
+      <div>
+         <Banner />
+        <Cart />
+        <ShoppingList />
+        <CareScale />
+        <QuestionForm />
+        <Footer />
+         </div>
+         )
+}
+
+export default App
